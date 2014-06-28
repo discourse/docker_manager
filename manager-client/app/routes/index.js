11096e77fc45451576d1c14b8def28d41d6962bb
Arpit Jalan <arpit@techapj.com>
2014-06-29 01:14:33 +0530

    Add alert if the tracking branch is master for Discourse

--- manager-client/app/routes/index.js
+++ manager-client/app/routes/index.js
@@ -23,8 +23,18 @@ export default Em.Route.extend({
 
   actions: {
     upgrade: function(repo) {
-      this.transitionTo('upgrade', repo);
+      var self = this;
+      // Special case: If the branch is "master" warn user
+      if (repo.get('id') === 'discourse' && repo.get('branch') === 'origin/master') {
+        bootbox.confirm('<p><b>WARNING:</b> Your Discourse instance is tracking "master" branch which may be unstable, we recommend tracking "tests-passed" branch.</p> <p>To continue upgrading press "OK".</p>', function(confirmed) {
+          if(confirmed) {
+            self.transitionTo('upgrade', repo);
+          }
+        });
+      } else {
+        self.transitionTo('upgrade', repo);
+      }
+
     }
   }
 });
-
