8ec46c850f7df73c293d285107b7921051818a16
Arpit Jalan <arpit@techapj.com>
2014-06-29 01:16:17 +0530

    Add compiled files

--- assets/docker-manager-app.js
+++ assets/docker-manager-app.js
@@ -317,7 +317,7 @@ define("docker-manager/models/repo",
 
       repoAjax: function(url, args) {
         args = args || {};
-        args.data = this.getProperties('path', 'version');
+        args.data = this.getProperties('path', 'version', 'branch');
         return ajax(url, args);
       },
 
@@ -434,7 +434,18 @@ define("docker-manager/routes/index",
 
       actions: {
         upgrade: function(repo) {
-          this.transitionTo('upgrade', repo);
+          var self = this;
+          // Special case: If the branch is "master" warn user
+          if (repo.get('id') === 'discourse' && repo.get('branch') === 'origin/master') {
+            bootbox.confirm('<p><b>WARNING:</b> Your Discourse instance is tracking "master" branch which may be unstable, we recommend tracking "tests-passed" branch.</p> <p>To continue upgrading press "OK".</p>', function(confirmed) {
+              if(confirmed) {
+                self.transitionTo('upgrade', repo);
+              }
+            });
+          } else {
+            self.transitionTo('upgrade', repo);
+          }
+
         }
       }
     });
