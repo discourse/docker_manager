91cb013cc460fc1d710d53607b451013525f0b21
William Karavites <wkaravites@gmail.com>
2014-11-19 19:36:04 -0500

    Inserted backticks to execute version commands
    Put in the backticks instead of the quotes to execute the commands to get the version numbers.

--- app/controllers/docker_manager/admin_controller.rb
+++ app/controllers/docker_manager/admin_controller.rb
@@ -75,8 +75,8 @@ module DockerManager
     def versions
       # Get the different version numbers of the programs used
       render json: {versions: {ruby: "#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}",
-                                postgresql: "postmaster -V",
-                                redis: "redis-server --version"}}
+                                postgresql: `postmaster -V`,
+                                redis: `redis-server --version`}}
     end
 
     def runaway_cpu
