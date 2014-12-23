99264f444b0215b31694c8fe4931918914048de1
Sam <sam.saffron@gmail.com>
2014-12-23 14:32:56 +1100

    add moment properly

--- manager-client/Brocfile.js
+++ manager-client/Brocfile.js
@@ -18,6 +18,7 @@ var app = new EmberApp({
 
 app.import("bower_components/bootbox/bootbox.js");
 app.import("bower_components/message-bus/assets/message-bus.js");
+app.import("vendor/moment/moment.js");
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
