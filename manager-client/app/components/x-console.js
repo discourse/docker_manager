82c061a5bc4a793dfd919e7271a2c6660b41f917
Robin Ward <robin.ward@gmail.com>
2016-07-11 16:39:18 -0400

    FIX: Wasn't escaping the content of the console
    Since end users can't populate this it doesn't seem like a security
    issue, but a recent commit message had an HTML tag in it that
    screwed up the rendering so it should be fixed.

--- manager-client/app/components/x-console.js
+++ manager-client/app/components/x-console.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
 
   render: function(buffer) {
     if (!Ember.isEmpty(this.get('output'))) {
-      buffer.push(this.get('output'));
+      buffer.push(Ember.Handlebars.Utils.escapeExpression(this.get('output')));
     }
   },
 
