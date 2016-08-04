e5de9b3bc429a942751c5459428fc4611875fe4f
cpradio <forums@cpradio.com>
2016-08-04 07:26:21 -0400

    FEATURE: Link to compare on GitHub for components needing to be upgraded

--- manager-client/app/helpers/fmt-commit.js
+++ manager-client/app/helpers/fmt-commit.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
-export default Ember.Handlebars.makeBoundHelper(function(sha1, url) {
+export default Ember.Handlebars.makeBoundHelper(function(sha1, url, branch) {
   if (Ember.isNone(url)) { return; }
-  return new Ember.Handlebars.SafeString("(<a href='" + url + "'>" + sha1 + "</a>)");
+  if (Ember.isNone(branch)) {
+    return new Ember.Handlebars.SafeString("(<a href='" + url + "'>" + sha1 + "</a>)");
+  }
+
+  var _url = url.substr(0, url.indexOf('.git'));
+  var _branch = (branch.indexOf('/') !== -1) ? branch.split('/')[1] : branch;
+  return new Ember.Handlebars.SafeString("(<a href='" + _url + "/compare/" + sha1 + "..." + _branch + "'>" + sha1 + "</a>)");
 });
