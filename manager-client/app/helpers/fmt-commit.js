0d39d098b16492beee8e721ea8b352233645809d
Robert O'Connor <rob@oconnor.ninja>
2016-09-10 00:41:37 -0400

    Open git comparison in new window

--- manager-client/app/helpers/fmt-commit.js
+++ manager-client/app/helpers/fmt-commit.js
@@ -3,10 +3,11 @@ import Ember from 'ember';
 export default Ember.Handlebars.makeBoundHelper(function(sha1, url, branch) {
   if (Ember.isNone(url)) { return; }
   if (Ember.isNone(branch)) {
-    return new Ember.Handlebars.SafeString("(<a href='" + url + "'>" + sha1 + "</a>)");
+    return new Ember.Handlebars.SafeString("(<a href='" + url + "' target='_new'>" + sha1 + "</a>)");
   }
 
   var _url = url.substr(0, url.indexOf('.git'));
   var _branch = (branch.indexOf('/') !== -1) ? branch.split('/')[1] : branch;
-  return new Ember.Handlebars.SafeString("(<a href='" + _url + "/compare/" + sha1 + "..." + _branch + "'>" + sha1 + "</a>)");
+  return new Ember.Handlebars.SafeString("(<a href='" + _url + "/compare/" +
+                                         sha1 + "..." + _branch + "' target='_new'>" + sha1 + "</a>)");
 });
