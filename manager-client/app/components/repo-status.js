d1e17711484252ed2b22a3522abb51f0db2704d0
Guo Xiang Tan <tgx_world@hotmail.com>
2018-05-16 14:50:44 +0800

    FIX: Official plugin image not working for subfolder.

--- manager-client/app/components/repo-status.js
+++ manager-client/app/components/repo-status.js
@@ -14,6 +14,11 @@ export default Ember.Component.extend({
     return true;
   }.property('upgradingRepo', 'repo', 'managerRepo', 'managerRepo.upToDate'),
 
+  officialRepoImageSrc: function() {
+    if (!this.get('repo.official')) return;
+    return Discourse.getURL("/plugins/docker_manager/images/font-awesome-check-circle.png");
+  }.property('repo.official'),
+
   actions: {
     upgrade: function() {
       this.sendAction('upgrade', this.get('repo'));
