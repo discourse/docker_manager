42bc80d0f385cf1c94627fa1d5391bb19c972591
Gerhard Schlager <mail@gerhard-schlager.at>
2018-11-16 15:44:52 +0100

    Remove old check for pngcrush version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
-require 'open3'
-
 module DockerManager::ApplicationHelper
-  def has_latest_pngcrush
-    cmd = 'pngcrush -version'
-    Open3.popen3(cmd) do |stdin, stdout, stderr|
-      pngcrush_version = stderr.read.strip.split(/\s+/)[1][0...-1]
-      @has_latest_pngcrush ||= Gem::Version.new(pngcrush_version) >= Gem::Version.new('1.7.85')
-    end
-  end
-
   def discourse_root_url
     Discourse.base_uri
   end
