950d8a783ae89b7ec7e11d040d488ae1b11d8810
Jarek Radosz <jradosz@gmail.com>
2020-07-22 11:02:21 +0200

    DEV: Prettier the .eslintrc.js file

--- manager-client/.eslintrc.js
+++ manager-client/.eslintrc.js
@@ -2,38 +2,32 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: "module"
   },
-  plugins: [
-    'ember'
-  ],
+  plugins: ["ember"],
   globals: {
     Em: false,
     MessageBus: false
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  extends: ["eslint:recommended", "plugin:ember/recommended"],
   env: {
     browser: true
   },
-  rules: {
-  },
+  rules: {},
   overrides: [
     // node files
     {
       files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js'
+        ".eslintrc.js",
+        ".template-lintrc.js",
+        "ember-cli-build.js",
+        "testem.js",
+        "blueprints/*/index.js",
+        "config/**/*.js",
+        "lib/*/index.js"
       ],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
         ecmaVersion: 2015
       },
       env: {
