956575de843842ba523ad25b2c3cd2d6d74e7490
Isaac Janzen <isaac.janzen@discourse.org>
2022-05-05 06:32:03 -0500

    DEV: Remove use of assign polyfill

--- manager-client/tests/helpers/start-app.js
+++ manager-client/tests/helpers/start-app.js
@@ -1,13 +1,12 @@
 import Application from "../../app";
 import config from "../../config/environment";
-import { assign } from "@ember/polyfills";
 import { run } from "@ember/runloop";
 
 export default function startApp(attrs) {
   let application;
 
   // use defaults, but you can override
-  let attributes = assign({}, config.APP, attrs);
+  let attributes = Object.assign({}, config.APP, attrs);
 
   run(() => {
     application = Application.create(attributes);
