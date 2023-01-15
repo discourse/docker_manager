59c20d9ddbd53f08af59f8c97e32518109a2a695
Jarek Radosz <jradosz@gmail.com>
2023-01-15 21:34:14 +0100

    DEV: Update routes

--- assets/javascripts/discourse/controllers/upgrade.js
+++ assets/javascripts/discourse/controllers/upgrade.js
@@ -1,141 +1,32 @@
-/* eslint-disable */
-
-/* global MessageBus, bootbox */
-import Repo from "discourse/plugins/docker_manager/discourse/models/repo";
 import Controller from "@ember/controller";
-import { equal } from "@ember/object/computed";
 import { computed } from "@ember/object";
+import getURL from "discourse-common/lib/get-url";
 
 export default Controller.extend({
-  output: null,
-
-  init() {
-    this._super();
-    this.reset();
-  },
-
-  complete: equal("status", "complete"),
-  failed: equal("status", "failed"),
-
-  multiUpgrade: computed("model.length", function () {
-    return this.get("model.length") !== 1;
-  }),
-
-  title: computed("model.@each.name", function () {
-    return this.get("multiUpgrade") ? "All" : this.get("model")[0].get("name");
-  }),
-
-  isUpToDate: computed("model.@each.upToDate", function () {
-    return this.get("model").every((repo) => repo.get("upToDate"));
-  }),
-
-  upgrading: computed("model.@each.upgrading", function () {
-    return this.get("model").some((repo) => repo.get("upgrading"));
-  }),
-
-  repos() {
-    const model = this.get("model");
-    return this.get("isMultiple") ? model : [model];
-  },
-
-  updateAttribute(key, value, valueIsKey = false) {
-    this.get("model").forEach((repo) => {
-      value = valueIsKey ? repo.get(value) : value;
-      repo.set(key, value);
-    });
-  },
-
-  messageReceived(msg) {
-    switch (msg.type) {
-      case "log":
-        this.set("output", this.get("output") + msg.value + "\n");
-        break;
-      case "percent":
-        this.set("percent", msg.value);
-        break;
-      case "status":
-        this.set("status", msg.value);
-
-        if (msg.value === "complete") {
-          this.get("model")
-            .filter((repo) => repo.get("upgrading"))
-            .forEach((repo) => {
-              repo.set("version", repo.get("latest.version"));
-            });
-        }
-
-        if (msg.value === "complete" || msg.value === "failed") {
-          this.updateAttribute("upgrading", false);
-        }
-
-        break;
+  showBanner: computed("banner", "bannerDismissed", "banner.[]", function() {
+    if (this.get("bannerDismissed")) {
+      return false;
     }
-  },
 
-  upgradeButtonText: computed("upgrading", function () {
-    if (this.get("upgrading")) {
-      return "Upgrading...";
-    } else {
-      return "Start Upgrading";
-    }
+    const banner = this.get("banner");
+    return banner && banner.length > 0;
   }),
 
-  startBus() {
-    MessageBus.subscribe("/docker/upgrade", (msg) => {
-      this.messageReceived(msg);
-    });
+  appendBannerHtml(html) {
+    const banner = this.get("banner") || [];
+    if (banner.indexOf(html) === -1) {
+      banner.pushObject(html);
+    }
+    this.set("banner", banner);
   },
 
-  stopBus() {
-    MessageBus.unsubscribe("/docker/upgrade");
-  },
-
-  reset() {
-    this.setProperties({ output: "", status: null, percent: 0 });
-  },
+  backupsUrl: computed(function() {
+    return getURL("/admin/backups");
+  }),
 
   actions: {
-    start() {
-      this.reset();
-
-      if (this.get("multiUpgrade")) {
-        this.get("model")
-          .filter((repo) => !repo.get("upToDate"))
-          .forEach((repo) => repo.set("upgrading", true));
-        return Repo.upgradeAll();
-      }
-
-      const repo = this.get("model")[0];
-      if (repo.get("upgrading")) {
-        return;
-      }
-      repo.startUpgrade();
-    },
-
-    resetUpgrade() {
-      bootbox.confirm(
-        "WARNING: You should only reset upgrades that have failed and are not running.\n\n" +
-          "This will NOT cancel currently running builds and should only be used as a last resort.",
-        (result) => {
-          if (result) {
-            if (this.get("multiUpgrade")) {
-              return Repo.resetAll(
-                this.get("model").filter((repo) => !repo.get("upToDate"))
-              ).finally(() => {
-                this.reset();
-                this.updateAttribute("upgrading", false);
-              });
-            }
-
-            const repo = this.get("model")[0];
-            repo.resetUpgrade().then(() => {
-              this.reset();
-            });
-          }
-        }
-      );
-    },
-  },
+    dismiss() {
+      this.set("bannerDismissed", true);
+    }
+  }
 });
-
-/* eslint-enable */
--- assets/javascripts/discourse/docker-manager-route-map.js
+++ assets/javascripts/discourse/docker-manager-route-map.js
@@ -2,9 +2,13 @@ export default {
   resource: "admin",
 
   map() {
-    this.route("docker-manager", { resetNamespace: true }, function () {
-      this.route("processes");
-      this.route("upgrade", { path: "/upgrade/:id" });
-    });
+    this.route(
+      "upgrade",
+      { resetNamespace: true, path: "upgrade" },
+      function () {
+        this.route("processes");
+        this.route("show", { path: "/:id" });
+      }
+    );
   },
 };
