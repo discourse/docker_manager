7ff7df8d73013efb23786b7b3e24a2f7189c68d9
Jarek Radosz <jradosz@gmail.com>
2023-01-16 00:41:45 +0100

    DEV: Add a missing backend subroute

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ DockerManager::Engine.routes.draw do
   scope "/admin", constraints: AdminConstraint.new do
     get "/docker", to: redirect("/admin/upgrade")
     get "/upgrade" => "admin#index"
+    get "/upgrade/:id" => "admin#index"
     get "/docker/repos" => "admin#repos"
     get "/docker/latest" => "admin#latest"
     get "/docker/progress" => "admin#progress"
