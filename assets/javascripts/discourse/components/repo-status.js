b0c85f1de7a29e48222250f9b05b376e672ecb86
Jarek Radosz <jradosz@gmail.com>
2023-01-16 10:33:08 +0100

    DEV: Modernize repo-status component

--- assets/javascripts/discourse/components/repo-status.js
+++ assets/javascripts/discourse/components/repo-status.js
@@ -1,49 +1,37 @@
-import Component from "@ember/component";
-import { computed } from "@ember/object";
+import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 
-export default Component.extend({
-  router: service(),
-  tagName: "",
+export default class RepoStatus extends Component {
+  @service router;
 
-  upgradeDisabled: computed(
-    "upgradingRepo",
-    "repo",
-    "managerRepo",
-    "managerRepo.upToDate",
-    function () {
-      const upgradingRepo = this.get("upgradingRepo");
-
-      if (!upgradingRepo) {
-        const managerRepo = this.get("managerRepo");
-        if (!managerRepo) {
-          return false;
-        }
-        return !managerRepo.get("upToDate") && managerRepo !== this.get("repo");
-      }
+  get upgradeDisabled() {
+    if (this.args.upgradingRepo) {
       return true;
     }
-  ),
 
-  officialRepoBadge: computed("repo.official", function () {
-    if (this.get("repo.fork")) {
+    if (!this.args.managerRepo) {
+      return false;
+    }
+
+    return (
+      !this.args.managerRepo.upToDate &&
+      this.args.managerRepo !== this.args.repo
+    );
+  }
+
+  get officialRepoBadge() {
+    if (this.args.repo.fork) {
       return "exclamation-circle";
-    } else if (this.get("repo.official")) {
+    } else if (this.args.repo.official) {
       return "check-circle";
     }
-  }),
+  }
 
-  officialRepoBadgeTitle: computed("repo.official", function () {
-    if (this.get("repo.fork")) {
+  get officialRepoBadgeTitle() {
+    if (this.args.repo.fork) {
       return "Forked Official Plugin";
-    } else if (this.get("repo.official")) {
+    } else if (this.args.repo.official) {
       return "Official Plugin";
     }
-  }),
-
-  actions: {
-    upgrade() {
-      this.get("router").transitionTo("upgrade", this.get("repo"));
-    },
-  },
-});
+  }
+}
