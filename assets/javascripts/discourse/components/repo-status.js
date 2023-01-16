ba418ede2fb06e639db964836fc4167afb2866a3
Jarek Radosz <jradosz@gmail.com>
2023-01-16 10:00:14 +0100

    DEV: Unused import

--- assets/javascripts/discourse/components/repo-status.js
+++ assets/javascripts/discourse/components/repo-status.js
@@ -1,7 +1,6 @@
 import Component from "@ember/component";
 import { computed } from "@ember/object";
 import { inject as service } from "@ember/service";
-import { getURLWithCDN } from "discourse-common/lib/get-url";
 
 export default Component.extend({
   router: service(),
