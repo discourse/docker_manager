36876a2db77229aa85f89f04df643f742457b5a5
Jarek Radosz <jradosz@gmail.com>
2023-02-15 17:50:46 +0100

    FEATURE: Restore long lost auto-refresh of `ps`

--- admin/assets/javascripts/discourse/routes/upgrade-processes.js
+++ admin/assets/javascripts/discourse/routes/upgrade-processes.js
@@ -1,11 +1,35 @@
 import ProcessList from "../models/process-list";
 import Route from "@ember/routing/route";
+import { bind } from "discourse-common/utils/decorators";
+import discourseLater from "discourse-common/lib/later";
+import { cancel } from "@ember/runloop";
+
+const REFRESH_INTERVAL = 5000;
 
 export default class UpgradeProcesses extends Route {
-  async model() {
-    const processes = new ProcessList();
-    await processes.refresh();
+  processes = null;
+  refreshTimer = null;
+  autoRefresh = false;
 
-    return processes;
+  model() {
+    this.processes = new ProcessList();
+    this.autoRefresh = true;
+    this.refresh();
+
+    return this.processes;
+  }
+
+  deactivate() {
+    this.autoRefresh = false;
+  }
+
+  @bind
+  async refresh() {
+    if (this.autoRefresh) {
+      await this.processes.refresh();
+      this.refreshTimer = discourseLater(this.refresh, REFRESH_INTERVAL);
+    } else {
+      cancel(this.refreshTimer);
+    }
   }
 }
