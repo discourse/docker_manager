b05864cc81f8f57748a992883d45f3dbf1c3ffb6
Vinoth Kannan <svkn.87@gmail.com>
2024-06-28 02:47:31 +0530

    UX: link repo SHA versions to the commit URLs

---
+++ admin/assets/javascripts/discourse/helpers/commit-url.js
@@ -0,0 +1,19 @@
+import { helper as buildHelper } from "@ember/component/helper";
+import { htmlSafe } from "@ember/template";
+
+export default buildHelper(function (params) {
+  const [_class, version, prettyVersion, url] = params;
+
+  if (!prettyVersion) {
+    return "";
+  }
+
+  if (!url) {
+    return prettyVersion;
+  }
+
+  const _url = url.substr(0, url.search(/(\.git)?$/));
+  const description = `<a class='${_class} commit-hash' title='${version}' href='${_url}/commit/${version}'>${prettyVersion}</a>`;
+
+  return new htmlSafe(description);
+});
