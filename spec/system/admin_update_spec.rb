f3eda0a1a9c09ac5a3763d914239798035427ec6
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2024-07-11 10:08:11 +0800

    DEV: Add debugging info to figure out why system test is failing on CI (#225)

--- spec/system/admin_update_spec.rb
+++ spec/system/admin_update_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe "Admin update", type: :system do
   it "displays the admin update page with the right respositories" do
     visit("/admin/update")
 
+    # We are getting a blank page on CI so adding debugging steps to figure out why
+    if ENV["CI"]
+      expect(page).to have_current_path("/admin/update")
+      puts page.html
+    end
+
     expect(page).to have_css("h3", exact_text: I18n.t("js.admin.docker.update_title"))
     expect(page).to have_css("tr.repo .repo__name", exact_text: "Discourse")
     expect(page).to have_css("tr.repo .repo__name", exact_text: "Docker Manager")
