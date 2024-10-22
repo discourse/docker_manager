f6e2c0c9a350f597f18ec0b45d5cfc7c836c2623
Ella <ella.estigoy@gmail.com>
2024-10-22 15:18:17 -0600

    DEV: update spec to use new classes

--- spec/system/admin_update_spec.rb
+++ spec/system/admin_update_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe "Admin update", type: :system do
     visit("/admin/update")
 
     expect(page).to have_css("h3", exact_text: I18n.t("js.admin.docker.update_title"))
-    expect(page).to have_css("tr.repo .repo__name", exact_text: "Discourse")
-    expect(page).to have_css("tr.repo .repo__name", exact_text: "Docker Manager")
-    expect(page).to have_css("tr.repo .repo__about a[href='https://meta.discourse.org/t/12655']")
+    expect(page).to have_css("tr.repo .d-admin-row__overview-name", exact_text: "Discourse")
+    expect(page).to have_css("tr.repo .d-admin-row__overview-name", exact_text: "Docker Manager")
+    expect(page).to have_css("tr.repo .d-admin-row__overview-about a[href='https://meta.discourse.org/t/12655']")
   ensure
     puts page.html if ENV["CI"]
   end
