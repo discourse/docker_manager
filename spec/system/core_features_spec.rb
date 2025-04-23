09e36dfc194375976f1dee5be1dda2033187c915
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2025-04-23 09:55:45 +0800

    DEV: Skip core features specs on stable (#269)
    The shared example is not available in stable yet

--- spec/system/core_features_spec.rb
+++ spec/system/core_features_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe "Core features", type: :system do
+  # TODO: Stop skipping when shared example is available in stable
+  next if Discourse.git_branch == "stable"
+
   before { enable_current_plugin }
 
   it_behaves_like "having working core features"
