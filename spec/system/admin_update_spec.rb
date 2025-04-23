6e4e95ef1ae11522964a69546b23a96747576623
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2025-04-23 09:55:45 +0800

    DEV: Skip core features specs on stable (#269)
    The shared example is not available in stable yet

--- spec/system/admin_update_spec.rb
+++ spec/system/admin_update_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Admin update", type: :system do
 
   before { sign_in(admin) }
 
-  xit "displays the admin update page with the right repositories" do
+  it "displays the admin update page with the right respositories" do
     visit("/admin/update")
 
     expect(page).to have_css("h3", exact_text: I18n.t("js.admin.docker.update_title"))
@@ -17,7 +17,5 @@ RSpec.describe "Admin update", type: :system do
     expect(page).to have_css(
       "tr.repo .d-admin-row__overview-about a[href='https://meta.discourse.org/t/12655']",
     )
-  ensure
-    puts page.html if ENV["CI"]
   end
 end
