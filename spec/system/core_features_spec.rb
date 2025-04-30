357efc19b0f03b56a71b1fa115f93cf36ebb96aa
Loïc Guitaut <loic@discourse.org>
2025-04-30 12:31:35 +0200

    DEV: Enable smoke specs again
    Now that `stable` provides the smoke specs, we can enable them again for
    this plugin.

--- spec/system/core_features_spec.rb
+++ spec/system/core_features_spec.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# TODO: Stop skipping when shared example is available in stable
-# RSpec.describe "Core features", type: :system do
-#   before { enable_current_plugin }
+RSpec.describe "Core features", type: :system do
+  before { enable_current_plugin }
 
-#   it_behaves_like "having working core features"
-# end
+  it_behaves_like "having working core features"
+end
