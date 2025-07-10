50ff0c0650a7aca0b6923d5443f7187b98bab1c3
David Taylor <david@taylorhq.com>
2025-07-10 11:14:47 +0100

    bump image to discourse/base:2.0.20250710-0019
    This will force a CLI rebuild for everyone, which is also important for the changes in https://meta.discourse.org/t/373574

--- app/controllers/docker_manager/admin_controller.rb
+++ app/controllers/docker_manager/admin_controller.rb
@@ -56,7 +56,7 @@ module DockerManager
           end
 
         version = Gem::Version.new(version)
-        expected_version = Gem::Version.new("2.0.20250114-0014")
+        expected_version = Gem::Version.new("2.0.20250710-0019")
         ruby_version = Gem::Version.new(RUBY_VERSION)
         expected_ruby_version = Gem::Version.new("3.2.1")
         min_stable_version = Gem::Version.new("3.0.0")
