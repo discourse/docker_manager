3db3a676bd8bd04f47ae7ba662e5e137092458b6
Loïc Guitaut <loic@discourse.org>
2025-11-25 17:54:56 +0100

    DEV: Add compatibility with Pitchfork

---
+++ lib/docker_manager/pitchfork_adapter.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module DockerManager
+  class PitchforkAdapter < WebServerAdapter
+    def server_name
+      "Pitchfork"
+    end
+
+    def launcher_pid
+      `pgrep -f unicorn_launcher`.strip.to_i
+    end
+
+    def master_pid
+      `pgrep -f "pitchfork monitor"`.strip.to_i
+    end
+
+    def workers
+      `pgrep -f -P #{master_pid} worker`.split("\n").map(&:to_i)
+    end
+  end
+end
---
+++ lib/docker_manager/unicorn_adapter.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module DockerManager
+  class UnicornAdapter < WebServerAdapter
+    def server_name
+      "Unicorn"
+    end
+
+    def launcher_pid
+      `pgrep -f unicorn_launcher`.strip.to_i
+    end
+
+    def master_pid
+      `pgrep -f "unicorn master -E"`.strip.to_i
+    end
+
+    def workers
+      `pgrep -f -P #{master_pid} worker`.split("\n").map(&:to_i)
+    end
+  end
+end
