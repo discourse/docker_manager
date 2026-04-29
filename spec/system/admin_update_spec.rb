da1b03dc3b0a2d78b26c2b99f01e366fab92e197
Jarek Radosz <jarek@cvx.dev>
2026-04-29 18:17:19 +0200

    DEV: Restore skipped admin update spec

--- spec/system/admin_update_spec.rb
+++ spec/system/admin_update_spec.rb
@@ -4,20 +4,17 @@ require_dependency "docker_manager/git_repo"
 
 RSpec.describe "Admin update" do
   fab!(:admin)
-  let(:admin_update_page) { PageObjects::Pages::AdminUpdate.new }
 
   before { sign_in(admin) }
 
-  xit "displays the admin update page with the right repositories" do
+  it "displays the admin update page with the right repositories" do
     visit("/admin/update")
 
-    expect(page).to have_css("h3", exact_text: I18n.t("js.admin.docker.update_title"))
+    expect(page).to have_css("h1", exact_text: I18n.t("js.admin.docker.update_title"))
     expect(page).to have_css("tr.repo .d-table__overview-name", exact_text: "Discourse")
-    expect(page).to have_css("tr.repo .d-table__overview-name", exact_text: "Docker Manager")
+    expect(page).to have_css("tr.repo .d-table__overview-name", exact_text: "Docker manager")
     expect(page).to have_css(
       "tr.repo .d-table__overview-about a[href='https://meta.discourse.org/t/12655']",
     )
-  ensure
-    puts page.html if ENV["CI"]
   end
 end
