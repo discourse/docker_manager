f5488549d6f981485175db426ddbea1c414b1c97
David Taylor <david@taylorhq.com>
2026-07-27 14:43:40 +0100

    Bump image to discourse/base:2.0.20260726-0220 (#327)
    - https://github.com/discourse/discourse_docker/commit/ae00b9f0e14a4f08c3e52b276fc367c840a2470c
    - https://github.com/discourse/discourse_docker/commit/8e01602ac395e3f27df6042ad2868c30ef0f0859

--- app/controllers/docker_manager/admin_controller.rb
+++ app/controllers/docker_manager/admin_controller.rb
@@ -56,7 +56,7 @@ module DockerManager
           end
 
         version = Gem::Version.new(version)
-        expected_version = Gem::Version.new("2.0.20260609-1222")
+        expected_version = Gem::Version.new("2.0.20260726-0220")
         ruby_version = Gem::Version.new(RUBY_VERSION)
         expected_ruby_version = Gem::Version.new("3.4.9")
         upgrade_image = version < expected_version
